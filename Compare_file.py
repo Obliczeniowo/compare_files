05e3c5765a1f78b939593cf43a16408a1d3d7078
Obliczeniowo <ZajaczkowskiKrzysztofKZ@gmail.com>
2016-12-22 12:54:53 +0100

    Little bug fixed

--- Compare_file.py
+++ Compare_file.py
@@ -59,10 +59,6 @@ def compareFile(file1, file2, maxdiff):
     return 0
 
 if os.path.exists(path) and os.path.isdir(path):
-    
-    if not path.endswith("/") or not path.endswith("\\"):
-        path += "/";
-    
     fl = list(os.walk(path))
 
     filelist = files = [i for i in list(os.walk(path))[0][2] if i.lower().endswith(".jpg")]
@@ -74,32 +70,37 @@ if os.path.exists(path) and os.path.isdir(path):
     nr = 1
     filelists = []
     for file1 in filelist[:-1]:
-        if os.path.exists(path + file1):
+        if os.path.exists(os.path.join(path, file1)):
             cmplist = []
             print("compare " + file1)
             for file2 in filelist[nr:]:
-                if os.path.exists(path + file2):
-                    if compareFile(path + file1, path + file2, 0) == 0:
+                if os.path.exists(os.path.join(path, file2)):
+                    if compareFile(os.path.join(path, file1), os.path.join(path, file2), 0) == 0:
                         print("\npowtarza sie " + file2 + " z " + file1 + "\n")
                         cmplist += [("powtarza sie " + file2 + " z " + file1, file2)];
+                else:
+                    print("Ścieżka nie istnieje")
             if len(cmplist):
                 filelists += [cmplist.copy()];
             nr += 1
+        else:
+            print("Ścieżka nie istnieje" + os.path.join(path, file1))
     
-    delete = False
-    
+    delete = True
+    deleted = 0
     if len(filelists):
-        data = open(path + "files.txt", "w", -1, "utf-8")
+        data = open(path + "files.txt", "w", encoding="utf-8")
         for i in filelists:
             for j in i:
                 data.write(str(j) + "\n")
             if delete:
                 for j in i:
-                    if os.path.exists(path + j[1]):
-                        os.remove(path + j[1])
+                    if os.path.exists(os.path.join(path, j[1])):
+                        os.remove(os.path.join(path, j[1]))
+                        deleted += 1
         data.write(str(filelists))
         data.close()
-        print("\nWykryto {nr} powtarzających się plików, ktore usunięto\n")
+        print("\nWykryto {nr} powtarzających się plików, ktore usunięto\n".format(nr = deleted))
     else:
         print("\nNie wykryto powtarzających się plików\n")
 else:
